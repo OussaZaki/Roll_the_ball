4a58a15ae46b853bf52cf197dd9152a78839d50c
Oussama ZAKI <zaki.oussama@gmail.com>
2014-11-11 11:45:37 +0000

    Roll it Babe

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+	
+	void Start(){
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+	
+	void FixedUpdate (){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); 
+		
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+	
+	void OnTriggerEnter(Collider other) {
+        //Destroy(other.gameObject);
+		if( other.gameObject.tag == "PickUp" ){
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText ();
+		}
+    }
+
+	void SetCountText ()
+	{
+		countText.text = "Score: " + count;
+		if ( count >= 8 ){
+			winText.text = "YOU WIN !!";
+		}
+	}
+}
